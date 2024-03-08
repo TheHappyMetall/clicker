6859106d5d7a3acf7e86431d45206bd7d11c2c04
TheHappyMetall <123291110+TheHappyMetall@users.noreply.github.com>
2024-03-08 22:19:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,4 +7,39 @@ clickHere.addEventListener("click", () => {
 
   localStorage.setItem("score", ++score);
   document.querySelector(".score").innerHTML = score;
+
+  switch (score) {
+    case 10:
+      alert("Первая десятка!");
+      break;
+
+    case 50:
+      alert("Ничего себе! 50 очков!");
+      break;
+
+    case 100:
+      alert("Вау! Сотка!");
+      break;
+
+    case 500:
+      alert("Кто ты? 0.о ЭТО ЖЕ 500 ОЧКОВ!");
+      break;
+  }
+});
+// ссылка на новости где умерло столько челоек, сколько накликано
+
+// Меню
+document.querySelector(".open-menu-btn").addEventListener("click", () => {
+  document.querySelector(".menu-wrap").classList.add("opened-menu");
+});
+document.querySelector(".close-menu-btn").addEventListener("click", () => {
+  document.querySelector(".menu-wrap").classList.remove("opened-menu");
+});
+
+// Внутри меню
+document.querySelector(".new-game-btn").addEventListener("click", () => {
+  if (confirm("Вы действительно хотите начать сначала?")) {
+    localStorage.removeItem("score");
+    window.location.reload();
+  }
 });
